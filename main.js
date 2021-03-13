39d57d8d176891af8750c0f359fa07cdba70c24e
billcoding <wangmiao07@sina.com>
2021-03-13 12:11:38 +0800

    Firsrt import

---
+++ main.js
@@ -0,0 +1,27 @@
+const { app, BrowserWindow, Menu } = require('electron')
+
+Menu.setApplicationMenu(null)
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 400,
+    height: 400,
+    resizable: false
+  })
+
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
